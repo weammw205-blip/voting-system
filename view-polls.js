d4b07ae97946f0c7603f5497517152435519cea9
weammw205-blip <weam.mw.205@gmail.com>
2026-05-08 20:43:40 +0300

    Update view-polls.js

--- view-polls.js
+++ view-polls.js
@@ -1,5 +1,4 @@
 const polls = JSON.parse(localStorage.getItem("polls")) || [
-<<<<<<< HEAD
     {
       id: 1,
       question: "Who is your favorite programming language?"
@@ -37,42 +36,3 @@ const polls = JSON.parse(localStorage.getItem("polls")) || [
   }
   
   displayPolls();
-=======
-  {
-    id: 1,
-    question: "Who is your favorite programming language?"
-  },
-  {
-    id: 2,
-    question: "Which study method do you prefer?"
-  },
-  {
-    id: 3,
-    question: "Do you support online voting systems?"
-  }
-];
-
-function displayPolls() {
-  const pollList = document.getElementById("pollList");
-  pollList.innerHTML = "";
-
-  polls.forEach(poll => {
-    const div = document.createElement("div");
-    div.classList.add("poll");
-
-    div.innerHTML = `
-      <h3>${poll.question}</h3>
-      <button onclick="openPoll(${poll.id})">Open Poll</button>
-    `;
-
-    pollList.appendChild(div);
-  });
-}
-
-function openPoll(id) {
-  localStorage.setItem("selectedPollId", id);
-  window.location.href = "vote.html";
-}
-
-displayPolls();
->>>>>>> e56094f9ef98406e706ad03d1098d871e373bfbd
